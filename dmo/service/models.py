5173db284e7fe8d555c0cbfc70052fbcb11f0c20
aburncooks <aburncooks@gmail.com>
2024-07-12 01:37:44 +0100

    Add profiles

--- dmo/service/models.py
+++ dmo/service/models.py
@@ -3,6 +3,8 @@ Various and sundry models
 """
 import os.path
 
+import yaml
+
 
 class Mods:
     """
@@ -16,12 +18,11 @@ class Mods:
         """
         self.config = config
         self.mods_path = self.config["mods_path"]
+        self.mods = []
 
-    def load_mods_from_folder(self) -> list:
+    def load_mods_from_mods_path_folder(self) -> None:
         """
         Get all the mods in the mods folder
-
-        :return: dict of mods
         """
         # TODO: what if the mods folder stops existing?
         new_mods = []
@@ -30,4 +31,59 @@ class Mods:
                 # TODO: what about .pk3s?
                 continue
             new_mods.append(os.path.join(self.mods_path, file))
-        return new_mods
+        self.mods = new_mods
+
+    def as_dict(self) -> dict:
+        """
+        Output this class as a dict
+
+        :return: mods as a dict
+        """
+        return {"config": self.config,
+                "mods_path": self.mods_path,
+                "mods": self.mods}
+
+
+class Profile:
+    """
+    Profile class
+    """
+    def __init__(self) -> None:
+        """
+        Create a Profile class
+        """
+        self.name = None
+        self.mods = []
+
+    def load_profile_from_file(self, file_path: str) -> None:
+        """
+        Load a profile from a file
+        """
+        with open(file_path, "r") as profile_yaml:
+            profile = yaml.safe_load(profile_yaml.read())
+            # TODO: what if this fails?
+
+        if "name" in profile.keys():
+            self.name = profile["name"]
+
+        if "mods" in profile.keys():
+            self.mods = profile["mods"]
+
+    def write_profile_to_file(self, file_path: str) -> None:
+        """
+        Create a YAML file from a profile
+        """
+        yaml_dict = self.as_dict()
+
+        with open(file_path, "w") as yaml_file:
+            # TODO: what if the path doesn't exist?
+            yaml.dump(yaml_dict, yaml_file)
+
+    def as_dict(self) -> dict:
+        """
+        Output this profile as a dict
+
+        :return: profile as a dict
+        """
+        return {"name": self.name,
+                "mods": self.mods}
